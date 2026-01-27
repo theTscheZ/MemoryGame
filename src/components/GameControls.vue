36bc68ca051f12c3473a5d77a390b366ee4931fb
theTscheZ <bandion88@gmail.com>
2026-01-27 15:22:37 +0100

    outsourced buttons into new vue

---
+++ src/components/GameControls.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps<{
+  size: number
+  sizeCap: number
+}>()
+
+const emit = defineEmits<{
+  (e: "stop"): void
+  (e: "next"): void
+  (e: "reset"): void
+}>()
+</script>
+
+<template>
+  <footer>
+    <button @click="emit('stop')">
+      Return to Menu
+    </button>
+
+    <button
+        @click="emit('next')"
+        :disabled="size >= sizeCap"
+    >
+      Make it harder
+    </button>
+
+    <button @click="emit('reset')">
+      Reset to Easy
+    </button>
+  </footer>
+</template>
